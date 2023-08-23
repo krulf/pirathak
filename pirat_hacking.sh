0fa84ea044f87787463538f4a6771bf278129788
Allan Lassesen <allan@Allan.lan>
2023-08-23 21:17:17 +0200

    Pull scipt added

--- pirat_hacking.sh
+++ pirat_hacking.sh
@@ -1,8 +1,12 @@
 #/bin/sh
 
 git clone https://github.com/krulf/pirathak.git
-sudo apt install -y glow
+
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg -y
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
 
 cd pirathak/Terminal-træning
-
+clear
 glow Introduktion.md
