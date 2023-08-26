0a320ccdc151282201a7ec876d4cca235e8c4723
krulf <jtietze@gmail.com>
2023-08-26 13:38:31 +0200

    Til archlinux

--- pirat_hacking-arch.sh
+++ pirat_hacking-arch.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # For arch linux
-
-git clone https://github.com/krulf/pirathak.git
+#
 
 sudo pacman -S glow
 
+git clone https://github.com/krulf/pirathak.git
+
+# Sæt hjem
+hjem=$(pwd)/pirathak
+
+# Sæt hjælp og nulstil til første opgave
+alias hjælp='glow $hjem/Terminal-træning/intro.md'
+alias start='clear && cd $hjem/Terminal-træning && glow intro.md'
+
 alias opgave3='echo "# Sådan det køre....\n\n### virker det..?" >> næste_opgave.md | glow næste_opgave.md'
 
-cd pirathak/Terminal-træning
-clear
-glow Introduktion.md
+start
