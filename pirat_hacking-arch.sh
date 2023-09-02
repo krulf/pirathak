ca9a2ee01b1773b806533362ce6570ee3e121eaf
Allan Lassesen <allan@Allan.lan>
2023-09-02 14:35:32 +0200

    Changed scipt to clone pirathavn branch

--- pirat_hacking-arch.sh
+++ pirat_hacking-arch.sh
@@ -4,8 +4,8 @@
 
 sudo pacman -S glow
 
-git clone https://github.com/krulf/pirathak.git
+git clone --branch pirathavn https://github.com/krulf/pirathak.git pirathavn
 
-source aliasdef
+source pirathavn/.aliasdef
 
 start
