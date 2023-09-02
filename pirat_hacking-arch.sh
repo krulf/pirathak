f0beb96f26985deecec507757bc9d4a2a1dfab75
krulf <jtietze@gmail.com>
2023-09-02 22:27:30 +0200

    update start

--- pirat_hacking-arch.sh
+++ pirat_hacking-arch.sh
@@ -6,6 +6,6 @@ sudo pacman -S --needed glow
 
 git clone --branch pirathavn https://github.com/krulf/pirathak.git pirathavn
 
-echo "Run the following:"
-echo "source aliasdef"
-echo "start"
+source aliasdef
+start
+$SHELL
