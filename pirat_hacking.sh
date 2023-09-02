41ea56c5ea16fff07750f88641b6cd1d0d9beedc
krulf <jtietze@gmail.com>
2023-09-02 22:32:27 +0200

    update start

--- pirat_hacking.sh
+++ pirat_hacking.sh
@@ -7,6 +7,7 @@ curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/ke
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 sudo apt update && sudo apt install glow
 
+bash
 source aliasdef
 start
 $SHELL
