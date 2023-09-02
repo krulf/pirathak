2021693806a8e67961c2e21a5f6084e5e82c58c4
krulf <jtietze@gmail.com>
2023-09-02 21:49:36 +0200

    removed -y from curl command

--- pirat_hacking.sh
+++ pirat_hacking.sh
@@ -3,7 +3,7 @@
 git clone https://github.com/krulf/pirathak.git
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg -y
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
 echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
 sudo apt update && sudo apt install glow
 
